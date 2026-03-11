8aa0c15a29a33754825f33374a592fbfa5872dec
isanda101 <sandaibrahim72@yahoo.com>
2026-03-11 10:16:05 -0400

    update sign in/out

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
-  return (
-    <div className="min-h-screen bg-white px-4 py-8 flex items-start justify-center">
-      <SignIn />
-    </div>
-  );
+  return <SignIn />;
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
-  return (
-    <div className="min-h-screen bg-white px-4 py-8 flex items-start justify-center">
-      <SignUp />
-    </div>
-  );
+  return <SignUp />;
 }
